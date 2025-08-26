a0770a28be708a7dc1b2ffa0a7c178f35fbefc93
Max <maximilien01993@gmail.com>
2025-08-26 11:30:19 +0200

    Add hidden class to install button for initial state management

--- install.js
+++ install.js
@@ -3,6 +3,7 @@ const installBtn = document.querySelector("#installBtn");
 
 let deferredPrompt = null;
 
+installBtn.classList.add('hidden');
 
 window.addEventListener('beforeinstallprompt', e => {
     e.preventDefault();
