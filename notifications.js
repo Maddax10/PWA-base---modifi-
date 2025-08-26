7f0849b850dd9227c6cbf6f67c50b1ee1f325c27
Max <maximilien01993@gmail.com>
2025-08-26 16:23:34 +0200

    Refactor notification handling to improve readability and functionality

--- notifications.js
+++ notifications.js
@@ -1,44 +1,37 @@
-const notification = () => {
-    const notifMeBtn = document.querySelector("#notifMe");
-
-    console.log("notifmeBtn", notifMeBtn)
-
-    notifMeBtn.addEventListener("click", (e) => {
-        e.preventDefault();
-
-        let myNotification = null;
+const notifications = () => {
+    const notifBtn = document.querySelector('#notifMe')
+    notifBtn.addEventListener('click', e => {
+        e.preventDefault()
+        let myNotification = null
 
         const options = {
             body: "Texte de la notif",
             icon: "/icons/favicon-16x16.png",
-            url: "https://cepegra.be",
-            vibrate: [200, 100, 200, 100, 200, 100, 200, 100, 200, 100, 200, 100],
-        };
-        //Si on a pas les droits
+            vibrate: [200, 100, 200, 100, 200, 100, 200]
+        }
         if (!("Notification" in window)) {
-            alert("Pas de notifs dispos dans ce navigateur");
-        }
-        //On a deja les droits ?
-        else if (Notification.permission === 'granted') {
-            console.log('Notif accepted');
-            myNotification = new Notification("Hello", options);
-            myNotification.open("https://cepegra.be", '_blank');
-        }
-        //si on a pas déjà demandé si on avait le droit, alors on demande
-        else {
+            alert('Pas de notifs dispos dans ce navigateur')
+        } else if (Notification.permission === 'granted') {
+            console.log('Notif accepted')
+            myNotification = new Notification("Hello", options)
+            myNotification.onclick = () => {
+                window.open("https://cepegra.be/", '_blank')
+            }
+        } else {
             Notification.requestPermission()
-                .then(permissionResp => {
-                    if (permissionResp === 'granted') {
-                        console.log('Notif accepted');
-                        myNotification = new Notification("Hello", options);
-                        myNotification.open("https://cepegra.be", '_blank');
+                .then(permission => {
+                    if (permission === 'granted') {
+                        console.log('Notif accepted')
+                        myNotification = new Notification("Hello", options)
+                        myNotification.onclick = () => {
+                            window.open("https://cepegra.be/", '_blank')
+                        }
                     }
                 })
         }
 
-        myNotification = null;
+        myNotification = null
+    })//onclick sur btn
+}
 
-    });
-};
-
-notification();
+notifications()
